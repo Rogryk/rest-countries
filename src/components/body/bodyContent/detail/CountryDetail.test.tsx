989d02f5febe1cca9e63b29bf77c3b78f0f777f9
Robert <robertgryko@outlook.com>
2022-10-04 10:00:35 +0200

    CountryDetail tests

--- src/components/body/bodyContent/detail/CountryDetail.test.tsx
+++ src/components/body/bodyContent/detail/CountryDetail.test.tsx
@@ -5,9 +5,7 @@ import CountryDetail from "./CountryDetail";
 import "../../../../../setupTest";
 
 describe("CountryDetail component", () => {
-  //   test("passes proper values to callback function", () => {
   test("renders all provided info", () => {
-    const callbackFunction = jest.fn((arg) => arg);
     render(
       <CountryDetail
         flag="dummy.html"
@@ -18,30 +16,60 @@ describe("CountryDetail component", () => {
         nativeName="testNative"
         subregion="testSubRegion"
         topLevelDomain="testTopLevelDomain"
-        currencies={[{ name: "testCurrencies" }, { name: "testCurrencies" }]}
-        languages={[{ name: "testLanguages" }]}
+        currencies={[{ name: "testCurrencies1" }, { name: "testCurrencies2" }]}
+        languages={[{ name: "testLanguages1" }, { name: "testLanguages2" }]}
+        borderCountries={["testBorderCountries1", "testBorderCountries2"]}
+        backClickHandler={() => {}}
+        countryClickHandler={() => {}}
+      />
+    );
+
+    expect(screen.getByAltText("flag of testName")).toBeInTheDocument;
+    expect(screen.getByText("testName")).toBeInTheDocument;
+    expect(screen.getByText("9,999")).toBeInTheDocument;
+    expect(screen.getByText("testRegion")).toBeInTheDocument;
+    expect(screen.getByText("testCapital")).toBeInTheDocument;
+    expect(screen.getByText("testNative")).toBeInTheDocument;
+    expect(screen.getByText("testSubRegion")).toBeInTheDocument;
+    expect(screen.getByText("testTopLevelDomain")).toBeInTheDocument;
+    expect(screen.getByText("testCurrencies1")).toBeInTheDocument;
+    expect(screen.getByText("testCurrencies2")).toBeInTheDocument;
+    expect(screen.getByText("testLanguages1")).toBeInTheDocument;
+    expect(screen.getByText("testLanguages2")).toBeInTheDocument;
+    expect(screen.getByText("testBorderCountries1")).toBeInTheDocument;
+    expect(screen.getByText("testBorderCountries2")).toBeInTheDocument;
+  });
+
+  test("passes proper values to callback function", () => {
+    const callbackFunction = jest.fn((arg) => arg);
+
+    render(
+      <CountryDetail
+        flag="dummy.html"
+        name="testName"
+        population={9999}
+        region="testRegion"
+        capital="testCapital"
+        nativeName="testNative"
+        subregion="testSubRegion"
+        topLevelDomain="testTopLevelDomain"
+        currencies={[{ name: "testCurrencies1" }, { name: "testCurrencies2" }]}
+        languages={[{ name: "testLanguages1" }, { name: "testLanguages2" }]}
         borderCountries={["testBorderCountries1", "testBorderCountries2"]}
         backClickHandler={() => {}}
         countryClickHandler={callbackFunction}
       />
     );
 
-    expect(screen.getByText("testName")).toBeInTheDocument;
-    expect(screen.getByText("testRegion")).toBeInTheDocument;
-    expect(screen.getByText("testCapital")).toBeInTheDocument;
-    expect(screen.getByText("testNative")).toBeInTheDocument;
-    expect(screen.getByText("testSubRegion")).toBeInTheDocument;
-    expect(screen.getByText("testTopLevelDomain")).toBeInTheDocument;
-    expect(screen.getByText("testCurrencies")).toBeInTheDocument;
-    expect(screen.getByText("testLanguages")).toBeInTheDocument;
-    expect(screen.getByText("testBorderCountries1")).toBeInTheDocument;
-    expect(screen.getByText("testBorderCountries2")).toBeInTheDocument;
-
-    // const inputField: HTMLInputElement = screen.getByRole("textbox");
-    // fireEvent.change(inputField, { target: { value: "test" } });
-    // fireEvent.change(inputField, { target: { value: "test text" } });
-
-    // expect(mockKeywordFilterHandler.mock.results[1].value).toEqual("test");
-    // expect(mockKeywordFilterHandler.mock.results[2].value).toEqual("test text");
+    const testBorderCountry1 = screen.getByText("testBorderCountries1");
+    const testBorderCountry2 = screen.getByText("testBorderCountries2");
+    fireEvent.click(testBorderCountry1);
+    fireEvent.click(testBorderCountry2);
+    expect(callbackFunction.mock.results[0].value).toEqual(
+      "testBorderCountries1"
+    );
+    expect(callbackFunction.mock.results[1].value).toEqual(
+      "testBorderCountries2"
+    );
   });
 });
