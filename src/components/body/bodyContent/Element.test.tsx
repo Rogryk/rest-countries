e7ec8dd6a4e0de2ce8b462f51074411853b6d059
Robert <robertgryko@outlook.com>
2022-10-05 12:53:31 +0200

    code cleaning

--- src/components/body/bodyContent/Element.test.tsx
+++ src/components/body/bodyContent/Element.test.tsx
@@ -3,7 +3,6 @@ import { fireEvent, render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import Element from "./Element";
 import "../../../../setupTest";
-import { strict } from "assert";
 
 describe("CountryDetail component", () => {
   test("renders all provided info", () => {
